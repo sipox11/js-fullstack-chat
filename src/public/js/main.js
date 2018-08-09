dcaf0342bae1655f47b7eb12ff8fcf85ecdbb1d1
Jose Carlos Montero Gómez <sipox11@gmail.com>
2018-08-09 23:32:20 +0200

    Implement private chat feature

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -6,18 +6,71 @@ $(() => {
     const $messageBox = $('#message');
     const $chat = $('#chat');
 
+    // Obtaining DOM elements from nickname form
+    const $nicknameForm = $('#nickname-form');
+    const $nicknameError = $('#nickname-error');
+    const $nickname = $('#nickname');
+
+    // Obtaining DOM elements from user list
+    const $users = $('#usernames');
+
     // Capture events
     // --> Handle message sending
     $messageForm.submit(e => {
         e.preventDefault();
         console.log('Sending data: ', $messageBox.val());
-        socket.emit('send_message', $messageBox.val());
+        socket.emit('send_message', $messageBox.val(), data => {
+            console.error(data);
+            $chat.append(`<p class="error">${data}</p>`);
+        });
         $messageBox.val('');
+    });
+
+    // --> Handle nickname registration 
+    $nicknameForm.submit(e => {
+        e.preventDefault();
+        $('#nickname-error').hide();
+        console.log('Registrating nickname: ', $nickname.val());
+        socket.emit('new_user', $nickname.val(), data => {
+            if(data) {
+                console.log('Success!');
+                $('#nickname-wrapper').hide();
+                $('#content-wrapper').show();
+            } else {
+                console.log('Already exists!');
+                $('#nickname-error').show();
+                $nicknameError.html(`
+                <div class="alert alert-danger mt-2">
+                    That username already exists!
+                </div>
+                `);
+            }
+            $nickname.val('');
+        });
     });
 
     // --> Handle message receipt
     socket.on('new_message', data => {
         console.log('Somebody sent a message: ', data);
-        $chat.append(data + '<br />');
+        $chat.append(`<strong>${data.nickname}:</strong> ${data.message}<br />`);
     });
+
+    // --> Handle username broadcast
+    socket.on('broadcast_usernames', nicknames => {
+        console.log(`Received nickname broadcast with data: ${nicknames}`);
+        let html = '';
+        for (let i = 0; i < nicknames.length; i++) {
+            html += `
+                <p><i class="fa fa-user"></i> ${nicknames[i]}</p>
+            `;
+        }
+        $users.html(html);
+    });
+
+    // --> Handle whisper event (private messages)
+    socket.on('whisper', data => {
+        console.log('Whisper received: ', data);
+        $chat.append(`<p class="whisper"><strong>${data.nickname}:</strong> ${data.message}</p>`);
+    })
+
 });
--- src/sockets.js
+++ src/sockets.js
@@ -1,12 +1,75 @@
-module.exports  = function(io) {
+module.exports = function(io) {
+    
+    let users = {
+
+    };
+
     io.on('connection', socket => {
         console.log('New client connected!');
 
         // Handle incoming messages
-        socket.on('send_message', data => {
+        socket.on('send_message', (data, callback) => {
             console.log('Data received: ', data);
-            // Retransmit a message to all sockets
-            io.sockets.emit('new_message', data);
+            // Process message looking for special commands
+            let cleanData = data.trim();
+            if(cleanData.substr(0,3) === '/w ') {
+                cleanData = cleanData.substr(3);
+                let index = cleanData.indexOf(' ');
+                if( index !== -1) {
+                    let name = cleanData.substr(0, index);
+                    cleanData = cleanData.substr(index + 1);
+                    if (name in users) {
+                        users[name].emit('whisper', {
+                            message: cleanData,
+                            nickname: socket.nickname
+                        });
+                        users[socket.nickname].emit('whisper', {
+                            message: cleanData,
+                            nickname: socket.nickname
+                        });
+                    } else {
+                        callback('ERROR! Please enter a valid user');
+                    }
+                } else {
+                    callback('ERROR! Please enter your message!');
+                }
+            } else {
+                // Retransmit a message to all sockets
+                let msg = {
+                message: data,
+                nickname: socket.nickname
+            };
+            io.sockets.emit('new_message', msg);
+            }
         });
+
+        // Handle nickname registration
+        socket.on('new_user', (data, callback) => {
+            console.log('New nickname registration received: ', data);
+            if(data in users) {
+                // User does exist
+                callback(false);
+            } else {
+                // User does not exist
+                callback(true);
+                socket.nickname = data;
+                users[socket.nickname] = socket;
+            }
+            console.log('Broadcasting usernames: ', users);
+            updateNicknames();
+        });
+
+        // Handle user disconnection
+        socket.on('disconnect', data => {
+            if(!socket.nickname) return;
+            delete users[socket.nickname]
+            updateNicknames();
+        });
+
+        
     });
+
+    function updateNicknames () {
+        io.sockets.emit('broadcast_usernames', Object.keys(users));
+    }
 };
